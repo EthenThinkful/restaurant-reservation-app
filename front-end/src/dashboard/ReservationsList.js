b60d5f65d6d78e32648fe1b9f2e427bc01487756
Ethen Roth <ethenroth@Ethens-MacBook-Air.local>
2022-09-12 14:29:28 -0600

    search page fully functional

--- front-end/src/dashboard/ReservationsList.js
+++ front-end/src/dashboard/ReservationsList.js
@@ -1,18 +1,17 @@
 import React from "react";
 import "./ReservationList.css";
-import { formatAsTime } from "../utils/date-time";
 
-function ReservationList({ reservation }) {
+function ReservationList({ reservation, formatTime }) {
   const { first_name, last_name, mobile_number, reservation_time, reservation_date, people, reservation_id, status } =
     reservation;
 
-  // let formattedTime = formatAsTime(reservation_time);
-  // let formattedHours = Number(formattedTime.slice(0,2)) > 12 ? Number(formattedTime.slice(0,2) % 12) : Number(formattedTime.slice(0,2));
-  // formattedTime = `${formattedHours}${formattedTime.slice(2)}`;
-  console.log("LINE 12", reservation)
+  let formattedTime = formatTime(reservation_time);
+  let formattedHours = Number(formattedTime.slice(0,2)) > 12 ? Number(formattedTime.slice(0,2) % 12) : Number(formattedTime.slice(0,2));
+  formattedTime = `${formattedHours}${formattedTime.slice(2)}`;
+  
   return (
     <div id="reservation-card">
-      {/* <div id="card-title">Reservation for {formattedTime} {new Date(`${reservation_date} ${reservation_time}`).getHours() < 12 ? "AM" : "PM"}</div> */}
+      <div id="card-title">Reservation for {formattedTime} {new Date(`${reservation_date} ${reservation_time}`).getHours() < 12 ? "AM" : "PM"}</div>
       <div className="text-center">
         <h6 id="card-label">Name:</h6>
         <p id="card-text">{first_name} {last_name}</p>
--- front-end/src/layout/Search.js
+++ front-end/src/layout/Search.js
@@ -1,115 +1,66 @@
-import React, { useEffect, useState } from "react";
-import { useHistory } from "react-router";
+import React, { useState } from "react";
 import ErrorAlert from "./ErrorAlert";
 import ReservationList from "../dashboard/ReservationsList";
-// import { formatAsTime } from "../utils/date-time";
+import { formatAsTime } from "../utils/date-time";
 
 const { REACT_APP_API_BASE_URL } = process.env;
 
 function Search() {
-  const history = useHistory();
-
-  const initialFormState = {
-    mobile_number: "",
-  };
-
-  const [formState, setFormState] = useState(initialFormState);
-  const [error, setError] = useState(null);
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [mobileData, setMobileData] = useState([]);
-
-  useEffect(() => {
-    const abortController = new AbortController();
-    setErrorMessage(error);
-    return () => abortController.abort();
-  }, [error]);
-
-  const cancelHandler = () => {
-    setFormState({ ...initialFormState });
-    history.goBack();
-  };
+  const [formState, setFormState] = useState("");
+  const [reservations, setReservations] = useState([]);
+  const [reservationsError, setReservationsError] = useState(null);
 
   function changeHandler({ target }) {
-    setFormState({ ...formState, mobile_number: target.value });
+    setFormState(target.value);
   }
 
-  const submitHandler = async (e) => {
+  async function submitHandler(e) {
     e.preventDefault();
-    console.log("FORM STATE", formState);
     const response = await fetch(
-      `${REACT_APP_API_BASE_URL}/reservations?mobile_number=${formState.mobile_number}`,
+      `${REACT_APP_API_BASE_URL}/reservations?mobile_number=${formState}`,
       {
         method: "GET",
         headers: {
           "Content-type": "application/json",
         },
-        //   body: JSON.stringify({data: formState}),
       }
     );
     const resData = await response.json();
-    console.log("RESDATA", resData);
-    setMobileData(resData);
-    if (resData.error) {
-      setError(resData.error);
+    console.log(resData)
+    console.log(resData.data.length)
+    if (response.status !== 400) {
+        setReservations(resData.data);
     }
-    // if (response.status !== 400) {
-    //   setFormState({ ...initialFormState });
-    //   history.push(`/dashboard/?date=${resData.data.reservation_date}`);
-    // }
-  };
-  console.log("MOBILE ZATA", mobileData)
-//   let matchedCards = mobileData.length ? (
-//     console.log("LINE 62", mobileData),
-//     mobileData.map((reservation, index) => {
-//     console.log("LOINE 64", reservation.reservation_id)
-//     return (
-//     <ReservationList
-//       key={index}
-//       reservation={reservation}
-//       formatTime={formatAsTime}
-//     />
-//     )
-// })) : (console.log("haha"))
+    if (resData.error) {
+        setReservationsError(resData.error)
+    }
+  }
+
+  const reservationsList = reservations.map((reservation, index) => {
+    return (
+      <ReservationList
+        key={index}
+        reservation={reservation}
+        formatTime={formatAsTime}
+      />
+    );
+  });
 
   return (
     <div>
-      {error ? <ErrorAlert errorMessage={errorMessage} /> : <></>}
-      <div className="form-group">
-        <form onSubmit={submitHandler}>
-          <label htmlFor="mobile_number">Reservation Contact Number</label>
-          <input
-            required
-            type="text"
-            id="mobile_number"
-            name="mobile_number"
-            placeholder="xxx-xxx-xxxx"
-            value={formState.mobile_number}
-            onChange={changeHandler}
-          ></input>
-          <br />
-          <button
-            type="button"
-            name="cancel-btn"
-            onClick={cancelHandler}
-            className="btn btn-primary"
-          >
-            Cancel
-          </button>
-          <button type="submit" name="submit-btn" className="btn btn-secondary">
-            Find
-          </button>
-        </form>
-        <ReservationList
-      key={mobileData.reservation_id}
-      reservation={mobileData}
-    //   formatTime={formatAsTime}
-    />
-        {/* {mobileData ? (
-          <div>
-            {matchedCards}
-          </div>
-        ) : "No reservations found"} */}
-      </div>
+        <div>{reservationsError ? <ErrorAlert errorMessage={reservationsError} /> : <></>}</div>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="mobile_number">Search</label>
+        <input
+          required
+          name="mobile_number"
+          id="mobile_number"
+          onChange={changeHandler}
+          placeholder="Enter a customer's phone number"
+        ></input>
+        <button type="submit">Find</button>
+      </form>
+      <div>{reservationsList.length === 0 ? <> "No reservations found" </> : reservationsList}</div>
     </div>
   );
 }
