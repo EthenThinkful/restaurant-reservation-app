3499fb807663e44a852460309dc2cb12dd1bfa58
Ethen Roth <ethenroth@Ethens-MacBook-Air.local>
2022-09-16 13:46:18 -0600

    made new form component and fixed tuesday reservations

--- front-end/src/layout/ReservationForm.js
+++ front-end/src/layout/ReservationForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router";
-import "./NewReservation.css";
+
 
 function ReservationForm({reservationData, setReservationData, submitHandler}) {
 
