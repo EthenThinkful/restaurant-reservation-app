9fb7f5ef6b2bd2c2205105304f2298eb87b3d07b
Ethen Roth <Ethenfm@gmail.com>
2022-12-21 18:55:21 -0700

    saving progress in restyling queries

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./front-end/src/**/*.{js, jsx, ts, tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  prefix: 'tw-',
+}
